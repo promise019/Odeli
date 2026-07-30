eb2a19438cf1490683cc1b7434e0bec9e78a2135
promise019 <edetpromise019@gmail.com>
2026-07-30 11:59:46 +0100

    feat: icon updated

--- frontend/build.mjs
+++ frontend/build.mjs
@@ -5,8 +5,8 @@ const watch = process.argv.includes("--watch");
 const ctx = await esbuild.context({
      entryPoints: {
         main: "src/main.ts",
-        settings: "src/settings.ts",
-        welcome: "src/welcome.ts",
+        // settings: "src/settings.ts",
+        // welcome: "src/welcome.ts",
     },
     outdir: "build",
     bundle: true,
@@ -15,6 +15,10 @@ const ctx = await esbuild.context({
     platform: "browser",
     target: "es2022",
     sourcemap: true,
+    loader: {
+    '.png': 'file', // Copies PNGs to the output folder and returns their path URL
+    // '.png': 'dataurl', // Alternatively: inlines the PNG as a Base64 string
+  },
 });
 
 if (watch) {
